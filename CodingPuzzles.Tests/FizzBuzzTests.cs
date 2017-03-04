9af63e1e5bc8214833adc914b7b2128fbb5dff25
LAPTOP-4E83PLLR\Sekhar Mamidi <sekhar.mamidi.97@gmail.com>
2017-03-03 21:55:13 -0500

    updated project

--- CodingPuzzles.Tests/FizzBuzzTests.cs
+++ CodingPuzzles.Tests/FizzBuzzTests.cs
@@ -7,8 +7,40 @@ namespace CodingPuzzles.Tests
     public class FizzBuzzTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void FizzBuzz_Int_StringNumber()
         {
+            FizzBuzz fb = new FizzBuzz();
+            Assert.AreEqual("7", fb.GetFizzBuzzResult(7));
         }
+
+        [TestMethod]
+        public void FizzBuzz_Three_StringFizz()
+        {
+            FizzBuzz fb = new FizzBuzz();
+            Assert.AreEqual("Fizz", fb.GetFizzBuzzResult(3));
+        }
+
+        [TestMethod]
+        public void FizzBuzz_DivisibleByThree_StringFizz()
+        {
+            FizzBuzz fb = new FizzBuzz();
+            Assert.AreEqual("Fizz", fb.GetFizzBuzzResult(39));
+        }
+
+        [TestMethod]
+        public void FizzBuzz_Five_StringFizz()
+        {
+            FizzBuzz fb = new FizzBuzz();
+            Assert.AreEqual("Buzz", fb.GetFizzBuzzResult(5));
+        }
+
+        [TestMethod]
+        public void FizzBuzz_DivisibleByFive_StringBuzz()
+        {
+            FizzBuzz fb = new FizzBuzz();
+            Assert.AreEqual("Buzz", fb.GetFizzBuzzResult(35));
+
+        }
+
     }
 }
--- CodingPuzzles/FizzBuzz.cs
+++ CodingPuzzles/FizzBuzz.cs
@@ -8,7 +8,44 @@ namespace CodingPuzzles
 {
     public class FizzBuzz
     {
-        
-           
+        public void CheckFizzBuzz()
+        {
+            for (int i = 1; i <= 1000; i++)
+            {
+                var result = GetFizzBuzzResult(i);
+                Console.WriteLine(result);
+            }
+        }
+
+        public string GetFizzBuzzResult(int i)
+        {
+            string result = string.Empty;
+            if ((i % 3 == 0) && (i % 5 == 0))
+            {
+                result = "FizzBuzz";
+            }
+
+            if (i % 3 == 0)
+            {
+                result = "Fizz";
+            }
+
+            if (i % 5 == 0)
+            {
+                result = "Buzz";
+            }
+
+            if (string.IsNullOrEmpty(result))
+            {
+                result = i.ToString();
+            }
+            return result;
+
+        }
+
     }
 }
+
+
+
+
