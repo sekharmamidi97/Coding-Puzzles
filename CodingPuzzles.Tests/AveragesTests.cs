aea84661183cf8dc4b742fee431d032507bcc6f9
LAPTOP-4E83PLLR\Sekhar Mamidi <sekhar.mamidi.97@gmail.com>
2017-03-04 22:14:39 -0500

    updated

---
+++ CodingPuzzles.Tests/AveragesTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodingPuzzles.Tests
+{
+    [TestClass]
+    public class AveragesTests
+    {
+        [TestMethod]
+        public void Array_Average_Less_Than_Fifty()
+        {
+            Averages avg = new Averages();
+            Assert.AreEqual(85, avg.AverageExercise1(50));
+        }
+
+        [TestMethod]
+        public void Array_Average_Less_Than_OneHundred()
+        {
+            Averages avg = new Averages();
+            Assert.AreEqual(206, avg.AverageExercise2(100));
+        }
+
+        [TestMethod]
+        public void Array_Average_Less_Than_OneHundredFifty()
+        {
+            Averages avg = new Averages();
+            Assert.AreEqual(317, avg.AverageExercise3(150));
+        }
+
+        [TestMethod]
+        public void Array_Average_Less_Than_TwoHundredSeventyFive()
+        {
+            Averages avg = new Averages();
+            Assert.AreEqual(458, avg.AverageExercise4(275));
+        }
+
+        [TestMethod]
+        public void Array_Average_Less_Than_FiveHundred()
+        {
+            Averages avg = new Averages();
+            Assert.AreEqual(261, avg.AverageExercise5(500));
+        }
+    }
+}
---
+++ CodingPuzzles/Averages.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingPuzzles
+{
+    public class Averages
+    {
+        public int AverageExercise1(int average)
+        {
+            int[] newArray = { 75, 62, 87, 190, 25, 76 };
+            average = (int)newArray.Average();
+
+            if (average < 50)
+            {
+                Console.WriteLine("The average is less than 50!");
+            }
+
+            else
+            {
+                Console.WriteLine("The average is greater than 50!");
+            }
+            return average;
+        }
+
+        public int AverageExercise2(int mean)
+        {
+            int[] numbersArray = { 65, 87, 276, 656, 89, 65 };
+            mean = (int)numbersArray.Average();
+
+            if (mean < 100)
+            {
+                Console.WriteLine("The mean is less than 100!");
+            }
+
+            else
+            {
+                Console.WriteLine("The mean is greater than 100!");
+            }
+            return mean;
+        }
+
+        public int AverageExercise3(int average)
+        {
+            int[] meanArray = { 56, 45, 197, 800, 54, 754 };
+            average = (int)meanArray.Average();
+
+            if (average < 150)
+            {
+                Console.WriteLine("The average is less than 150!");
+            }
+
+            else
+            {
+                Console.WriteLine("The average is greater than 150!");
+            }
+            return average;
+
+        }
+
+        public int AverageExercise4(int mean)
+        {
+            int[] averageArray = { 200, 750, 1000, 455, 222, 125 };
+            mean = (int)averageArray.Average();
+
+            if (mean < 275)
+            {
+                Console.WriteLine("The mean is less than 275");
+            }
+
+            else
+            {
+                Console.WriteLine("The mean is greater than 275");
+            }
+            return mean;
+        }
+
+        public int AverageExercise5(int average)
+        {
+            int[] arrayAverage = { 78, 425, 10, 62, 954, 42 };
+            average = (int)arrayAverage.Average();
+
+            if (average < 500)
+            {
+                Console.WriteLine("The average is less than 500!");
+               
+            }
+
+            else
+            {
+                Console.WriteLine("The average is greater than 500!");
+               
+            }
+            return average;
+        }
+    }
+}
