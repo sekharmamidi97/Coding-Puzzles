8f4dbc7ce80016ebe23c47cea802acd43c121b54
LAPTOP-4E83PLLR\Sekhar Mamidi <sekhar.mamidi.97@gmail.com>
2017-03-08 00:20:07 -0500

    updated

---
+++ CodingPuzzles.Tests/LeastCommonMultipleTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodingPuzzles.Tests
+{
+    [TestClass]
+    public class LeastCommonMultipleTests
+    {
+        [TestMethod]
+        public void LeastCommonMultiple_Eight_And_TwentyFour()
+        {
+            
+        }
+    }
+}
---
+++ CodingPuzzles/LeastCommonMultiple.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingPuzzles
+{
+   public class LeastCommonMultiple
+    {
+        public static int FindLeastCommonMultiple(int s, int m)
+        {
+            int number1, number2;
+            if (s > m)
+            {
+                number1 = s;
+                number2 = m;
+            }
+
+            else
+            {
+                number1 = m;
+                number2 = s;
+            }
+
+            for (int i = 1; i < number2; i++)
+            {
+                if ((number1 * i) % number2 == 0)
+                {
+                    return i * number1;
+                }
+
+            }
+            return number1 * number2;
+        }
+    }
+}
