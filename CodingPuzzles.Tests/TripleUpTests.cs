3457a8b4c6244b1e31f4305ad19d06c21e46e7e1
LAPTOP-4E83PLLR\Sekhar Mamidi <sekhar.mamidi.97@gmail.com>
2017-03-10 23:42:57 -0500

    Added TripleUp Exercise and TripleUp Test

---
+++ CodingPuzzles.Tests/TripleUpTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodingPuzzles.Tests
+{
+    [TestClass]
+    public class TripleUpTests
+    {
+        [TestMethod]
+        public void TripleUpArrayTest()
+        {
+            //Act
+            TripleUp tu = new TripleUp();
+
+            //Arrange
+            int[] arrayOne = new int[] { 1, 2, 3 };
+            int[] arrayTwo = new int[] { 23, 24, 25 };
+            int[] arrayThree = new[] { 57, 58, 60 };
+
+            //Assert
+            Assert.AreEqual(true, tu.TripleUpArray(arrayOne));
+            Assert.AreEqual(true, tu.TripleUpArray(arrayTwo));
+            Assert.AreEqual(false, tu.TripleUpArray(arrayThree));
+        }
+    }
+}
---
+++ CodingPuzzles/TripleUp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingPuzzles
+{
+    public class TripleUp
+    {
+        /*
+       Return true if the array contains, somewhere, three increasing adjacent numbers like 
+       .... 4, 5, 6, ... or 23, 24, 25.
+
+       tripleUp([1, 4, 5, 6, 2]) → true
+       tripleUp([1, 2, 3]) → true
+       tripleUp([1, 2, 4]) → false
+       */
+        public bool TripleUpArray(int[] nums)
+        {
+            bool answer = false;  
+            for (int i = 0; i < nums.Length - 2; i++)
+            {
+                if ((nums[i] + 1 == nums[i + 1]) && (nums[i + 1] + 1 == nums[i + 2]))
+                {
+                    answer= true;
+                }
+            }
+            return answer;
+        }
+    }
+}
