e3246c93cd9725e937f2809bdeab608dc9d5d1ae
LAPTOP-4E83PLLR\Sekhar Mamidi <sekhar.mamidi.97@gmail.com>
2017-03-12 23:22:59 -0400

    Adding Sort Array Exercise and Sort Array Test

---
+++ CodingPuzzles.Tests/SortTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodingPuzzles.Tests
+{
+    [TestClass]
+    public class SortTest
+    {
+        [TestMethod]
+        public void Array_Sorted()
+        {
+            Sort s = new Sort();
+
+            CollectionAssert.AreEquivalent(new int[] { 8, 13, 9, 12 }, s.ArraySort(new int[] { 8, 9, 12, 13 }));
+        }
+    }
+}
---
+++ CodingPuzzles/Sort.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingPuzzles
+{
+    public class Sort
+    {
+        /*
+        Given an integer array, return the array sorted lowest to highest.
+        CHALLENGE: Write the Sort algorithm, don't use the .Sort() method provided to an array. 
+
+        sort([8, 13, 9, 12]) → [8, 9, 12, 13]        
+        */
+        public int[] ArraySort(int[] nums)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    nums[i] = nums[i] + nums[j];
+                    nums[j] = nums[i] - nums[j];
+                    nums[i] = nums[i] - nums[j];
+                }
+            }
+            return nums;
+        }
+    }
+}
