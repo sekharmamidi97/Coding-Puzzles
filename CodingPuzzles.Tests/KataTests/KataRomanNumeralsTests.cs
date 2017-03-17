9dd50de55ce2fb8f8b75a1285e6b3c3308c4ecb3
LAPTOP-4E83PLLR\Sekhar Mamidi <sekhar.mamidi.97@gmail.com>
2017-03-17 00:21:47 -0400

    Kata Roman Numerals Exercise and Tests in Progress; need to makke the code better

---
+++ CodingPuzzles.Tests/KataTests/KataRomanNumeralsTests.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodingPuzzles.Katas;
+
+namespace CodingPuzzles.Tests.KataTests
+{
+    [TestClass]
+    public class KataRomanNumeralsTests
+    {
+        [TestMethod]
+        public void Int_1_To_RomanNumeral_I()
+        {
+            KataRomanNumerals krn = new KataRomanNumerals();
+            Assert.AreEqual("I", krn.RomanNumerals(1));
+            
+        }
+
+        [TestMethod]
+        public void Int_2_To_RomanNumeral_II()
+        {
+            KataRomanNumerals krn = new KataRomanNumerals();
+            Assert.AreEqual("II", krn.RomanNumerals(2));
+        }
+
+        [TestMethod]
+        public void Int_4_To_RomanNumeral_IV()
+        {
+            KataRomanNumerals krn = new KataRomanNumerals();
+            Assert.AreEqual("IV", krn.RomanNumerals(4));
+        }
+
+        [TestMethod]
+        public void Int_5_To_RomanNumeral_V()
+        {
+            KataRomanNumerals krn = new KataRomanNumerals();
+            Assert.AreEqual("V", krn.RomanNumerals(5));
+        }
+
+        [TestMethod]
+        public void Int_8_To_RomanNumeral_VIII()
+        {
+            KataRomanNumerals krn = new KataRomanNumerals();
+            Assert.AreEqual("VIII", krn.RomanNumerals(8));
+        }
+
+        [TestMethod]
+        public void Int_9_To_RomanNumeral_IX()
+        {
+            KataRomanNumerals krn = new KataRomanNumerals();
+            Assert.AreEqual("IX", krn.RomanNumerals(9));
+        }
+
+        [TestMethod]
+        public void Int_10_To_RomanNumeral_X()
+        {
+            KataRomanNumerals krn = new KataRomanNumerals();
+            Assert.AreEqual("X", krn.RomanNumerals(10));
+        }
+
+        [TestMethod]
+        public void Int_13_To_RomanNumeral_XIII()
+        {
+            KataRomanNumerals krn = new KataRomanNumerals();
+            Assert.AreEqual("XIII", krn.RomanNumerals(13));
+        }
+
+        [TestMethod]
+        public void Int_15_To_RomanNumeral_XV()
+        {
+            KataRomanNumerals krn = new KataRomanNumerals();
+            Assert.AreEqual("XV", krn.RomanNumerals(15));
+        }
+
+        [TestMethod]
+        public void Int_18_To_RomanNumeral_XVIII()
+        {
+            KataRomanNumerals krn = new KataRomanNumerals();
+            Assert.AreEqual("XVIII", krn.RomanNumerals(18));
+        }
+
+        [TestMethod]
+        public void Int_20_To_RomanNumeral_XX()
+        {
+            KataRomanNumerals krn = new KataRomanNumerals();
+            Assert.AreEqual("XX", krn.RomanNumerals(20));
+        }
+
+        [TestMethod]
+        public void Int_22_To_RomanNumeral_XXII()
+        {
+            KataRomanNumerals krn = new KataRomanNumerals();
+            Assert.AreEqual("XXII", krn.RomanNumerals(22));
+        }
+
+        [TestMethod]
+        public void Int_25_To_RomanNumeral_XXV()
+        {
+            KataRomanNumerals krn = new KataRomanNumerals();
+            Assert.AreEqual("XXV", krn.RomanNumerals(25));
+        }
+
+        [TestMethod]
+        public void Int_29_To_RomanNumeral_XXIX()
+        {
+            KataRomanNumerals krn = new KataRomanNumerals();
+            Assert.AreEqual("XXIX", krn.RomanNumerals(29));
+        }
+
+        [TestMethod]
+        public void Int_30_To_RomanNumeral_XXX()
+        {
+            KataRomanNumerals krn = new KataRomanNumerals();
+            Assert.AreEqual("XXX", krn.RomanNumerals(30));
+        }
+
+        [TestMethod]
+        public void Int_33_To_RomanNumeral_XXXIII()
+        {
+            KataRomanNumerals krn = new KataRomanNumerals();
+            Assert.AreEqual("XXXIII", krn.RomanNumerals(33));
+        }
+
+        [TestMethod]
+        public void Int_35_To_RomanNumeral_XXXV()
+        {
+            KataRomanNumerals krn = new KataRomanNumerals();
+            Assert.AreEqual("XXXV", krn.RomanNumerals(35));
+        }
+
+        [TestMethod]
+        public void Int_39_To_RomanNumeral_XXXIX()
+        {
+            KataRomanNumerals krn = new KataRomanNumerals();
+            Assert.AreEqual("XXXIX", krn.RomanNumerals(39));
+        }
+
+        [TestMethod]
+        public void Int_40_To_RomanNumeral_LX()
+        {
+            KataRomanNumerals krn = new KataRomanNumerals();
+            Assert.AreEqual("LX", krn.RomanNumerals(40));
+        }
+
+        [TestMethod]
+        public void Int_50_To_RomanNumeral_L()
+        {
+            KataRomanNumerals krn = new KataRomanNumerals();
+            Assert.AreEqual("L", krn.RomanNumerals(50));
+        }
+    }
+}
---
+++ CodingPuzzles/Katas/KataRomanNumerals.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingPuzzles.Katas
+{
+    public class KataRomanNumerals
+    {
+        public string RomanNumerals(int i)
+        {
+            if (i == 2)
+            {
+                return "II";
+            }
+
+            else if (i == 4)
+            {
+                return "IV";
+            }
+
+            else if (i == 5)
+            {
+                return "V";
+            }
+
+            else if (i == 8)
+            {
+                return "VIII";
+            }
+
+            else if (i == 9)
+            {
+                return "IX";
+            }
+
+            else if (i == 10)
+            {
+                return "X";
+            }
+
+            else if (i == 13)
+            {
+                return "XIII";
+            }
+
+            else if (i == 15)
+            {
+                return "XV";
+            }
+
+            else if (i == 18)
+            {
+                return "XVIII";
+            }
+
+            else if (i == 20)
+            {
+                return "XX";
+            }
+
+            else if (i == 22)
+            {
+                return "XXII";
+            }
+
+            else if (i == 25)
+            {
+                return "XXV";
+            }
+
+            else if (i == 29)
+            {
+                return "XXIX";
+            }
+
+            else if (i == 30)
+            {
+                return "XXX";
+            }
+
+            else if (i == 33)
+            {
+                return "XXXIII";
+            }
+
+            else if (i == 35)
+            {
+                return "XXXV";
+            }
+
+            else if (i == 39)
+            {
+                return "XXXIX";
+            }
+
+            else if (i == 40)
+            {
+                return "LX";
+            }
+
+            else if (i == 50)
+            {
+                return "L";
+            }
+
+            else
+            {
+                return "I";
+            }
+        }
+    }
+}
