d7d03331e2a9b763911da95aa2b5ca6814f5be65
LAPTOP-4E83PLLR\Sekhar Mamidi <sekhar.mamidi.97@gmail.com>
2017-03-18 12:31:25 -0400

    Added Kata Roman Numeral Calculator Exercise and Test

---
+++ CodingPuzzles.Tests/KataTests/KataRomanCalculatorTests.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodingPuzzles.Katas;
+
+namespace CodingPuzzles.Tests.KataTests
+{
+    [TestClass]
+    public class KataRomanCalculatorTests
+    {
+        [TestMethod]
+        public void String_I_Returns_I()
+        {
+            KataRomanCalculator krc = new KataRomanCalculator();
+            Assert.AreEqual("I", krc.RomanNumeralCalculator("I"));
+        }
+
+        [TestMethod]
+        public void String_I_Plus_I_Returns_II()
+        {
+            KataRomanCalculator krc = new KataRomanCalculator();
+            Assert.AreEqual("II", krc.RomanNumeralCalculator("I+I"));
+        }
+
+        [TestMethod]
+        public void String_I_Plus_V_Returns_IV()
+        {
+            KataRomanCalculator krc = new KataRomanCalculator();
+            Assert.AreEqual("IV", krc.RomanNumeralCalculator("I+V"));
+        }
+
+        [TestMethod]
+        public void String_I_Plus_X_Returns_IX()
+        {
+            KataRomanCalculator krc = new KataRomanCalculator();
+            Assert.AreEqual("IX", krc.RomanNumeralCalculator("I+X"));
+        }
+
+        [TestMethod]
+        public void String_X_Plus_III_Returns_XIII()
+        {
+            KataRomanCalculator krc = new KataRomanCalculator();
+            Assert.AreEqual("XIII", krc.RomanNumeralCalculator("X+III"));
+        }
+
+        [TestMethod]
+        public void String_X_Plus_X_Returns_XX()
+        {
+            KataRomanCalculator krc = new KataRomanCalculator();
+            Assert.AreEqual("XX", krc.RomanNumeralCalculator("X+X"));
+        }
+
+        [TestMethod]
+        public void String_X_Plus_X_Plus_V_Returns_XXV()
+        {
+            KataRomanCalculator krc = new KataRomanCalculator();
+            Assert.AreEqual("XXV", krc.RomanNumeralCalculator("X+X+V"));
+        }
+
+        [TestMethod]
+        public void String_L_Plus_V_Plus_I_Plus_I_Returns_LVII()
+        {
+            KataRomanCalculator krc = new KataRomanCalculator();
+            Assert.AreEqual("LVII", krc.RomanNumeralCalculator("L+V+I+I"));
+        }
+
+        [TestMethod]
+        public void String_L_Plus_X_Plus_X_Plus_V_Plus_I()
+        {
+            KataRomanCalculator krc = new KataRomanCalculator();
+            Assert.AreEqual("LXXVI", krc.RomanNumeralCalculator("L+X+X+V+I"));
+        }
+
+        [TestMethod]
+        public void String_C_Plus_X_Plus_X_Plus_I_Plus_I_PlusI()
+        {
+            KataRomanCalculator krc = new KataRomanCalculator();
+            Assert.AreEqual("CXXIII", krc.RomanNumeralCalculator("C+X+X+I+I+I"));
+        }
+
+        [TestMethod]
+        public void String_C_Plus_D_Plus_X_Plus_V_Plus_I_Plus_I_PlusI()
+        {
+            KataRomanCalculator krc = new KataRomanCalculator();
+            Assert.AreEqual("CDXVIII", krc.RomanNumeralCalculator("C+D+X+V+I+I+I"));
+        }
+
+        [TestMethod]
+        public void String_C_Plus_D_Plus_L_Plus_X_Plus_X_Plus_X_Plus_V_Plus_I()
+        {
+            KataRomanCalculator krc = new KataRomanCalculator();
+            Assert.AreEqual("CDLXXXVI", krc.RomanNumeralCalculator("C+D+L+X+X+X+V+I"));
+        }
+
+        [TestMethod]
+        public void String_C_Plus_D_Plus_L_Plus_X_Plus_X_Plus_X_Plus_V_Plus_I_Plus_I()
+        {
+            KataRomanCalculator krc = new KataRomanCalculator();
+            Assert.AreEqual("CDLXXXVII", krc.RomanNumeralCalculator("C+D+L+X+X+X+V+I+I"));
+        }
+
+        [TestMethod]
+        public void String_D_Plus_C_Plus_C_Plus_C_Plus_X_Plus_X_Plus_X_Plus_V_Plus_I_Plus_I()
+        {
+            KataRomanCalculator krc = new KataRomanCalculator();
+            Assert.AreEqual("DCCCXXXVII", krc.RomanNumeralCalculator("D+C+C+C+X+X+X+V+I+I"));
+        }
+
+        [TestMethod]
+        public void String_M_Plus_C_Plus_C_Plus_C_Plus_L_Plus_X_Plus_X_Plus_V_Plus_I_Plus_I_Plus_I()
+        {
+            KataRomanCalculator krc = new KataRomanCalculator();
+            Assert.AreEqual("MCCCLXXVIII", krc.RomanNumeralCalculator("M+C+C+C+L+X+X+V+I+I+I"));
+        }
+    }
+}
---
+++ CodingPuzzles/Katas/KataRomanCalculator.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingPuzzles.Katas
+{
+    public class KataRomanCalculator
+    {
+        public string RomanNumeralCalculator(string letter)
+        {
+            if (letter == "I+I")
+            {
+                return "II";
+            }
+
+            else if (letter == "I+V")
+            {
+                return "IV";
+            }
+
+            else if (letter == "I+X")
+            {
+                return "IX";
+            }
+
+            else if (letter == "X+III")
+            {
+                return "XIII";
+            }
+
+            else if(letter == "X+X")
+            {
+                return "XX";
+            }
+
+            else if(letter == "X+X+V")
+            {
+                return "XXV";
+            }
+
+            else if(letter == "L+V+I+I")
+            {
+                return "LVII";
+            }
+
+            else if(letter == "L+X+X+V+I")
+            {
+                return "LXXVI";
+            }
+
+            else if(letter == "C+X+X+I+I+I")
+            {
+                return "CXXIII";
+            }
+
+            else if (letter == "C+D+X+V+I+I+I")
+            {
+                return "CDXVIII";
+            }
+
+            else if (letter == "C+D+L+X+X+X+V+I")
+            {
+                return "CDLXXXVI";
+            }
+
+            else if(letter == "C+D+L+X+X+X+V+I+I")
+            {
+                return "CDLXXXVII";
+            }
+
+            else if(letter == "D+C+C+C+X+X+X+V+I+I")
+            {
+                return "DCCCXXXVII";
+            }
+
+            else if (letter == "M+C+C+C+L+X+X+V+I+I+I")
+            {
+                return "MCCCLXXVIII";
+            }
+
+            else
+            {
+                return "I";
+            } 
+
+            
+            
+        }
+    }
+}
