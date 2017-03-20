a3bd482fca265b0d940961f8e6fd9bf5aedc2058
LAPTOP-4E83PLLR\Sekhar Mamidi <sekhar.mamidi.97@gmail.com>
2017-03-20 00:02:30 -0400

    KataNumbersToWords Exercise and Test in Progress, will continue working on

---
+++ CodingPuzzles.Tests/KataTests/KataNumbersToWordsTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodingPuzzles.Katas;
+
+namespace CodingPuzzles.Tests.KataTests
+{
+    [TestClass]
+    public class KataNumbersToWordsTests
+    {
+        [TestMethod]
+        public void Number_1_To_String_One()
+        {
+            KataNumbersToWords ntw = new KataNumbersToWords();
+            Assert.AreEqual("one", ntw.NumbersToWords(1));
+
+        }
+    }
+}
---
+++ CodingPuzzles/Katas/KataNumbersToWords.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingPuzzles.Katas
+{
+    public class KataNumbersToWords
+    {
+        public string NumbersToWords(int number)
+        {
+            Dictionary<int, string> values = new Dictionary<int, string>()
+            {
+                {0, "zero" },
+                {1, "one" },
+                {2, "two" },
+                {3, "three" },
+                {4, "four" },
+                {5, "five" },
+                {6, "six" },
+                {7, "seven" },
+                {8, "eight" },
+                {9, "nine" },
+                {10, "ten" },
+                {20, "twenty" },
+                {30, "thirty" },
+                {40, "forty" },
+                {50, "fifty" },
+                {60, "sixty" },
+                {70, "seventy" },
+                {80, "eighty" },
+                {90, "ninety" },
+                
+            };
+
+            if (number >= 1000)
+            {
+                return NumbersToWords(number / 1000) + "thousand " + NumbersToWords(number % 1000);
+            }
+
+            if (number >= 100)
+            {
+                return NumbersToWords(number / 100) + "hundred " + NumbersToWords(number % 100);
+            }
+
+            if (number >= 21)
+            {
+                return NumbersToWords(number / 10 * 10) + " " + NumbersToWords(number % 10);
+            }
+
+            return NumbersToWords(number).ToString();
+        }
+    }
+}
